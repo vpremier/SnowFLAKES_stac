89b4a10316ea65412334aebb2fc3c8939b2e0504
Premier Valentina <vpremier@eosao38.eurac.edu>
2026-08-03 09:42:33 +0200

    Nothing to commit

--- main.py
+++ main.py
@@ -236,8 +236,8 @@ def run_workflow(date_start, date_end, config_path):
 
 if __name__ == "__main__":
 
-    start = pd.Timestamp("2020-12-12")
-    end = pd.Timestamp("2020-12-20")
+    start = pd.Timestamp("2020-04-01")
+    end = pd.Timestamp("2024-03-31")
 
     
 
@@ -248,7 +248,7 @@ if __name__ == "__main__":
     # config_path = './config/config_snowcop.json'
     # config_path = './config/config_snowcop_landsat.json'
 
-    config_path = './config/config_rofental_Sentinel2.json'
+    config_path = './config/config_senales.json'
 
 
     
