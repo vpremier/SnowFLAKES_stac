a414678e9a0112f0686c7115461268476ddbf29a
Premier Valentina <vpremier@eosao38.eurac.edu>
2026-09-08 11:54:55 +0200

    Updating notebook with new image

--- main.py
+++ main.py
@@ -236,8 +236,8 @@ def run_workflow(date_start, date_end, config_path):
 
 if __name__ == "__main__":
 
-    start = pd.Timestamp("2025-03-25")
-    end = pd.Timestamp("2025-03-31")
+    start = pd.Timestamp("2015-10-01")
+    end = pd.Timestamp("2025-09-30")
 
     
 
@@ -248,7 +248,7 @@ if __name__ == "__main__":
     # config_path = './config/config_snowcop.json'
     # config_path = './config/config_snowcop_landsat.json'
 
-    config_path = './config/config_southtyrol.json'
+    config_path = './config/config_ultental.json'
 
 
     
